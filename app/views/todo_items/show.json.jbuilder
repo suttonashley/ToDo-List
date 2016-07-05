95f72258b9a315b038aa9e1447b4bd4516d065b9
Ashley <ashleymsutton@outlook.com>
2016-07-05 11:36:57 -0400

    Scaffold rails

---
+++ app/views/todo_items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @todo_item, :id, :description, :done, :due, :created_at, :updated_at
---
+++ db/migrate/20160705153159_create_todo_items.rb
@@ -0,0 +1,11 @@
+class CreateTodoItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :todo_items do |t|
+      t.string :description
+      t.boolean :done
+      t.datetime :due
+
+      t.timestamps
+    end
+  end
+end
